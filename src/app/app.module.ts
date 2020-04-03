263df7356a42f192ab9f7565b4b95ae196a490fe
José Antonio López Lorenzo <jall.informatico@gmail.com>
2020-04-03 21:36:24 +0200

    añadido bootstrap

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,10 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { registerLocaleData } from '@angular/common';
+import localeES from '@angular/common/locales/es';
+registerLocaleData(localeES, 'es-Es');
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
